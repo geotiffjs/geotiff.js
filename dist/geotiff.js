fc174854d02409e0040ffc7d685eb3a11525bd80
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2015-10-29 17:47:12 +0100

    Updating build.

--- dist/geotiff.js
+++ dist/geotiff.js
@@ -750,25 +750,26 @@ module.exports = {
 
 },{}],4:[function(require,module,exports){
 
-var parse = function(data) {
-  var rawData;
-  if (typeof data === "string" || data instanceof String) {
-    rawData = new ArrayBuffer(data.length * 2); // 2 bytes for each char
-    var view = new Uint16Array(rawData);
-    for (var i=0, strLen=data.length; i<strLen; i++) {
-      view[i] = data.charCodeAt(i);
+(function(exports) {
+  exports.parse = function(data) {
+    var rawData;
+    if (typeof data === "string" || data instanceof String) {
+      rawData = new ArrayBuffer(data.length * 2); // 2 bytes for each char
+      var view = new Uint16Array(rawData);
+      for (var i=0, strLen=data.length; i<strLen; i++) {
+        view[i] = data.charCodeAt(i);
+      }
     }
-  }
-  else if (data instanceof ArrayBuffer) {
-    rawData = data;
-  }
-  else {
-    throw new Error("Invalid input data given.");
-  }
-  var GeoTIFF = require("./geotiff.js");
-  return new GeoTIFF(rawData);
-};
+    else if (data instanceof ArrayBuffer) {
+      rawData = data;
+    }
+    else {
+      throw new Error("Invalid input data given.");
+    }
+    var GeoTIFF = require("./geotiff.js");
+    return new GeoTIFF(rawData);
+  };
 
+})((typeof window !== "undefined") ? window["GeoTIFF"] = {} : exports);
 
-window.GeoTIFF = {parse: parse};
 },{"./geotiff.js":1}]},{},[4]);
