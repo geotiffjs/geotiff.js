e6f1de58df885067429c69c0f725f6ff7bc7302b
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-01-11 12:42:26 +0100

    To reduce the size of the output file removed all occurrences of Buffer
    disabled non-working LZW and Deflate decoders.

--- src/compression/deflate.js
+++ src/compression/deflate.js
@@ -1,35 +1,38 @@
 import AbstractDecoder from "../abstractdecoder.js"
 
+/*
 var Buffer = require('buffer');
 var inflate = require('inflate');
 var through = require('through');
+*/
 
 export default class DeflateDecoder extends AbstractDecoder {
   decodeBlockAsync(buffer, callback) {
-    through(function (data) {
-      this.queue(new Buffer(new Uint8Array(buffer)));
-    },
-    function() {
-      this.queue(null);
-    })
-    .pipe(inflate())
-    /*.pipe(function() {
-      alert(arguments);
-    })*/
-    .on("data", function(data) {
-      buffers.push(data);
-    })
-    .on("end", function() {
-      var buffer = Buffer.concat(buffers);
-      var arrayBuffer = new ArrayBuffer(buffer.length);
-      var view = new Uint8Array(ab);
-      for (var i = 0; i < buffer.length; ++i) {
-          view[i] = buffer[i];
-      }
-      callback(null, arrayBuffer);
-    })
-    .on("error", function(error) {
-      callback(error, null)
-    });
+    // through(function (data) {
+    //   this.queue(new Buffer(new Uint8Array(buffer)));
+    // },
+    // function() {
+    //   this.queue(null);
+    // })
+    // .pipe(inflate())
+    // /*.pipe(function() {
+    //   alert(arguments);
+    // })*/
+    // .on("data", function(data) {
+    //   buffers.push(data);
+    // })
+    // .on("end", function() {
+    //   var buffer = Buffer.concat(buffers);
+    //   var arrayBuffer = new ArrayBuffer(buffer.length);
+    //   var view = new Uint8Array(ab);
+    //   for (var i = 0; i < buffer.length; ++i) {
+    //       view[i] = buffer[i];
+    //   }
+    //   callback(null, arrayBuffer);
+    // })
+    // .on("error", function(error) {
+    //   callback(error, null)
+    // });
+    throw new Error("DeflateDecoder is not yet implemented.");
   }
 }
--- src/compression/lzw.js
+++ src/compression/lzw.js
@@ -1,9 +1,10 @@
 import AbstractDecoder from "../abstractdecoder.js"
-var lzwCompress = require("lzwcompress");
+//var lzwCompress = require("lzwcompress");
 
 
 export default class LZWDecoder extends AbstractDecoder {
   decodeBlock(buffer) {
-    return lzwCompress.unpack(Array.prototype.slice.call(new Uint8Array(buffer)));
+    throw new Error("LZWDecoder is not yet implemented");
+    //return lzwCompress.unpack(Array.prototype.slice.call(new Uint8Array(buffer)));
   }
 }
