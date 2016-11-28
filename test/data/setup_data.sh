e84cccdf7e8e3abc6c4733962651093a2d3645ca
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-11-28 09:30:47 +0100

    Added LZW image with predictor.

--- test/data/setup_data.sh
+++ test/data/setup_data.sh
@@ -13,6 +13,7 @@ gdal_translate -of GTiff -co INTERLEAVE=BAND stripped.tiff interleave.tiff
 gdal_translate -of GTiff -co TILED=YES -co BLOCKXSIZE=32 -co BLOCKYSIZE=32 -co INTERLEAVE=BAND stripped.tiff tiledplanar.tiff
 gdal_translate -of GTiff -co COMPRESS=LZW -co TILED=YES -co BLOCKXSIZE=32 -co BLOCKYSIZE=32 -co INTERLEAVE=BAND stripped.tiff tiledplanarlzw.tiff
 gdal_translate -of GTiff -co COMPRESS=LZW -ot Float64 stripped.tiff float64lzw.tiff
+gdal_translate -of GTiff -co COMPRESS=LZW -co PREDICTOR=2 stripped.tiff lzw_predictor.tiff
 gdal_translate -of GTiff -outsize 10% 10% stripped.tiff small.tiff
 
 # overviews
