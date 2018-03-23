d1187803af9704bc0241e1c92f6b551a715736c9
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2018-03-23 17:58:53 +0100

    Reducing main file. WiP.

--- src/main.js
+++ src/main.js
@@ -1,42 +1,45 @@
 import GeoTIFF from './geotiff';
 import { makeFetchSource } from './source';
 
-/**
- * Main parsing function for GeoTIFF files.
- * @param {(string|ArrayBuffer)} data Raw data to parse the GeoTIFF from.
- * @param {Object} [options] further options.
- * @param {Boolean} [options.cache=false] whether or not decoded tiles shall be cached.
- * @returns {GeoTIFF} the parsed geotiff file.
- */
-async function parse(source, options) {
+// /**
+//  * Main parsing function for GeoTIFF files.
+//  * @param {(string|ArrayBuffer)} data Raw data to parse the GeoTIFF from.
+//  * @param {Object} [options] further options.
+//  * @param {Boolean} [options.cache=false] whether or not decoded tiles shall be cached.
+//  * @returns {GeoTIFF} the parsed geotiff file.
+//  */
+// async function parse(source, options) {
 
-  return await GeoTIFF.parse(source, options)
+//   return await GeoTIFF.parse(source, options)
 
-  // let rawData;
-  // let i;
-  // let strLen;
-  // let view;
-  // if (typeof data === 'string' || data instanceof String) {
-  //   rawData = new ArrayBuffer(data.length * 2); // 2 bytes for each char
-  //   view = new Uint16Array(rawData);
-  //   for (i = 0, strLen = data.length; i < strLen; ++i) {
-  //     view[i] = data.charCodeAt(i);
-  //   }
-  // } else if (data instanceof ArrayBuffer) {
-  //   rawData = data;
-  // } else {
-  //   throw new Error('Invalid input data given.');
-  // }
-  // return new GeoTIFF(rawData, options);
-}
+//   // let rawData;
+//   // let i;
+//   // let strLen;
+//   // let view;
+//   // if (typeof data === 'string' || data instanceof String) {
+//   //   rawData = new ArrayBuffer(data.length * 2); // 2 bytes for each char
+//   //   view = new Uint16Array(rawData);
+//   //   for (i = 0, strLen = data.length; i < strLen; ++i) {
+//   //     view[i] = data.charCodeAt(i);
+//   //   }
+//   // } else if (data instanceof ArrayBuffer) {
+//   //   rawData = data;
+//   // } else {
+//   //   throw new Error('Invalid input data given.');
+//   // }
+//   // return new GeoTIFF(rawData, options);
+// }
 
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-  module.exports.parse = parse;
-}
-if (typeof window !== 'undefined') {
-  window.GeoTIFF = { parse, makeFetchSource };
-} else if (typeof self !== 'undefined') {
-  self.GeoTIFF = { parse, makeFetchSource };
-}
+// if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+//   module.exports.parse = parse;
+// }
+// if (typeof window !== 'undefined') {
+//   window.GeoTIFF = { parse, makeFetchSource };
+// } else if (typeof self !== 'undefined') {
+//   self.GeoTIFF = { parse, makeFetchSource };
+// }
 
-GeoTIFF.makeFetchSource = makeFetchSource;
+// GeoTIFF.makeFetchSource = makeFetchSource;
+
+
+export default GeoTIFF;
