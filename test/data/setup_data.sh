9f748a4baa490460dbe6c6b79e8e26a575b8be7f
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2018-05-15 22:25:04 +0200

    Fix travis (#44)
    * Trying to fix testing issues by using more recent Linux version.
    * Using different tool to generate YCbCr for testing.
    * Improving test data generation script.
    * Disabling test, as on travis the CMYK images are not 'similar' to RGB.

--- test/data/setup_data.sh
+++ test/data/setup_data.sh
@@ -30,7 +30,8 @@ rm BigTIFFSamples.zip
 
 # color images
 rgb2pct.py rgb.tiff rgb_paletted.tiff
-convert rgb.tiff -colorspace YCbCr ycbcr.tif
+# convert rgb.tiff -colorspace YCbCr ycbcr.tif
+rgb2ycbcr rgb.tiff ycbcr.tif -h 1 -v 1
 convert rgb.tiff -colorspace CMYK cmyk.tif
 convert rgb.tiff -colorspace Lab cielab.tif
 
