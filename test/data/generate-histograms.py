743e1cfc3f2f6b745ee83cd05b022e518fa0c354
DanielJDufour <daniel.j.dufour@gmail.com>
2018-12-26 18:51:04 +0000

    added generate-histograms.py and the histograms it generates for testing

---
+++ test/data/generate-histograms.py
@@ -0,0 +1,51 @@
+from collections import Counter
+import json
+import numpy as np
+from os import listdir
+from os.path import join
+import rasterio as rio
+from sys import argv
+
+def getHistograms(filepath, window=None):
+  histograms = {}
+  with rio.open(filepath) as src:
+    bands = src.read()
+    for bandindex, band in enumerate(bands):
+      counts = Counter()
+      if window:
+        left, top, right, bottom = window
+        for row in band[top:bottom]:
+          counts.update(row[left:right])
+      else:
+        for row in band:
+          counts.update(row)
+    cleaned = dict([(int(key), value) for key, value in counts.items()])
+    histograms[bandindex] = cleaned
+  return histograms
+
+# generate histograms for performNBitTests
+histograms = {}
+for n in range(1, 33):
+  histograms[n] = getHistograms(str(n) + '-bit-stripped.tif')
+
+# generate histograms for performTiffTests
+tiffs = [
+  "stripped.tiff",
+  "tiled.tiff",
+  "interleave.tiff",
+  "lzw.tiff",
+  "tiledplanarlzw.tiff",
+  "int32.tiff",
+  "uint32.tiff",
+  "float32.tiff",
+  "float64.tiff",
+  "float64lzw.tiff",
+  "packbits.tiff",
+  "bigtiff.tiff"
+]
+for filename in tiffs:
+  histograms[filename] = getHistograms(filename, [200, 200, 210, 210])
+
+
+with open("histograms.json", "w") as f:
+  f.write(json.dumps(histograms))
