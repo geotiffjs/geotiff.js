28341f030b8abd13e3fd19eba8beb26457b12954
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2021-01-21 15:55:55 +0100

    Add support for WebP decompression via the browser

--- src/compression/index.js
+++ src/compression/index.js
@@ -3,6 +3,7 @@ import LZWDecoder from './lzw';
 import JpegDecoder from './jpeg';
 import DeflateDecoder from './deflate';
 import PackbitsDecoder from './packbits';
+import WebImageDecoder from './webimage';
 
 export function getDecoder(fileDirectory) {
   switch (fileDirectory.Compression) {
@@ -20,6 +21,8 @@ export function getDecoder(fileDirectory) {
       return new DeflateDecoder();
     case 32773: // packbits
       return new PackbitsDecoder();
+    case 50001:
+      return new WebImageDecoder();
     default:
       throw new Error(`Unknown compression method identifier: ${fileDirectory.Compression}`);
   }
