246fc8433d56d2534f219b2ccb6e4dd602f3fd7d
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2021-09-17 09:13:44 +0200

    Fixing build issue

--- src/decoder.worker.js
+++ src/decoder.worker.js
@@ -1,7 +1,7 @@
 import { expose, Transfer } from 'threads/worker';
 import { getDecoder } from './compression';
 
-async async function decode(fileDirectory, buffer) {
+async function decode(fileDirectory, buffer) {
   const decoder = await getDecoder(fileDirectory);
   const decoded = await decoder.decode(fileDirectory, buffer);
   return Transfer(decoded);
