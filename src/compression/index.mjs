b41201ba60795eada730444ac6da2925d64c49a2
Trevor Manz <trevor.j.manz@gmail.com>
2021-09-24 15:53:02 -0400

    feat: remove @babel dependency

--- src/compression/index.mjs
+++ src/compression/index.mjs
@@ -15,10 +15,10 @@ export async function getDecoder(fileDirectory) {
 }
 
 // Add default decoders to registry (end-user may override with other implementations)
-addDecoder([undefined, 1], () => import('./raw').then(m => m.default));
-addDecoder(5,              () => import('./lzw').then(m => m.default));
+addDecoder([undefined, 1], () => import('./raw.mjs').then(m => m.default));
+addDecoder(5,              () => import('./lzw.mjs').then(m => m.default));
 addDecoder(6,              () => { throw new Error('old style JPEG compression is not supported.') });
-addDecoder(7,              () => import('./jpeg').then(m => m.default));
-addDecoder([8, 32946],     () => import('./deflate').then(m => m.default));
-addDecoder(32773,          () => import('./packbits').then(m => m.default));
-addDecoder(34887,          () => import('./lerc').then(m => m.default));
+addDecoder(7,              () => import('./jpeg.mjs').then(m => m.default));
+addDecoder([8, 32946],     () => import('./deflate.mjs').then(m => m.default));
+addDecoder(32773,          () => import('./packbits.mjs').then(m => m.default));
+addDecoder(34887,          () => import('./lerc.mjs').then(m => m.default));
