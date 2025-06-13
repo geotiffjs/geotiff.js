000d4f539c5bbd2fc7869513be5aea2828510fa3
Rdataflow <waldgis@bafu.admin.ch>
2025-06-13 11:44:46 +0200

    switch to zstddec/stream

--- src/compression/zstd.js
+++ src/compression/zstd.js
@@ -1,10 +1,10 @@
-import { ZSTDDecoder } from 'zstddec';
+import { ZSTDDecoder } from 'zstddec/stream';
 import BaseDecoder from './basedecoder.js';
 
 export const zstd = new ZSTDDecoder();
 
 export default class ZstdDecoder extends BaseDecoder {
   decodeBlock(buffer) {
-    return zstd.decode(new Uint8Array(buffer), 1000_000_000).buffer;
+    return zstd.decode(new Uint8Array(buffer)).buffer;
   }
 }
