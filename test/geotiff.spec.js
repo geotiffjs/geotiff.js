8cd3ad159efcd5605d50542c4916b2e825ec2132
Jonathan Chemla <jos.chemla@gmail.com>
2025-09-09 21:40:33 +0200

    Move geotiffwriter spec test GeoAsciiParams/GeoDoubleParams into geotiff tests

--- test/geotiff.spec.js
+++ test/geotiff.spec.js
@@ -1287,6 +1287,33 @@ describe('writeTests', () => {
     expect(normalize(fileDirectory.StripByteCounts)).to.equal(normalize(metadata.StripByteCounts));
     expect(fileDirectory.GDAL_NODATA).to.equal('0\u0000');
   });
+
+  it('should write and read back GeoAsciiParams/GeoDoubleParams keys', async () => {
+    const width = 2;
+    const height = 2;
+    const data = new Float32Array(width * height).fill(1);
+
+    const metadata = {
+      width,
+      height,
+      GeographicTypeGeoKey: 4326,
+      GTModelTypeGeoKey: 2,
+      GeogSemiMajorAxisGeoKey: 6378137.0, // DOUBLE
+      GeogInvFlatteningGeoKey: 298.257223563, // DOUBLE
+      GeogCitationGeoKey: 'WGS 84', // ASCII
+      PCSCitationGeoKey: 'test-ascii', // ASCII
+    };
+
+    const buffer = await writeArrayBuffer(data, metadata);
+    const tiff = await fromArrayBuffer(buffer);
+    const image = await tiff.getImage();
+
+    const geoKeys = image.getGeoKeys();
+    expect(geoKeys.GeogSemiMajorAxisGeoKey).to.be.closeTo(6378137.0, 0.000001);
+    expect(geoKeys.GeogInvFlatteningGeoKey).to.be.closeTo(298.257223563, 0.000001);
+    expect(geoKeys.GeogCitationGeoKey).to.equal('WGS 84');
+    expect(geoKeys.PCSCitationGeoKey).to.equal('test-ascii');
+  });
 });
 
 describe('BlockedSource Test', () => {
